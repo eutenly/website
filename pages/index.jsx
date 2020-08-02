fd3e794d9ff58b42aa490d24b235b93644e9ecb0
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-01 21:55:04 -0700

    Added voter perks page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "../components/Head";
 import NavBar from "../components/NavBar";
-import "./index.scss";
+import "./global.scss";
 
 export default () => (
     <div id="index">
@@ -26,8 +26,8 @@ export default () => (
                     <p className="description">Eutenly is a universal search bot for Discord. Search Google, YouTube, Twitter, Reddit, and a ton more services.</p>
                     <p className="description">We integrate with services allowing for rich displays of information, right in Discord.</p>
 
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
-                    <a className="support-server-button" href="/support-server" target="_blank">Support Server</a>
+                    <a className="button" href="/invite">Invite Eutenly</a>
+                    <a className="button-2" href="/support-server" target="_blank">Support Server</a>
 
                 </div>
             </div>
@@ -113,8 +113,8 @@ export default () => (
                     <p className="description">Search Google right from Discord with Eutenly's Google Command. Search results are embedded in an organized and elegant way, making it easy to view information. From there, you can easily get more info about any result.</p>
                     <p className="description">Eutenly even supports knowledge panels, song and artist info, lyrics, related questions, and a whole lot more.</p>
 
-                    <p className="ready-to-try">Ready to try it?</p>
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text">Ready to try it?</p>
+                    <a className="button" href="/invite">Invite Eutenly</a>
 
                 </div>
 
@@ -136,8 +136,8 @@ export default () => (
                     <p className="description">Search for YouTube videos, channels, and playlists with the YouTube Command. Eutenly uses YouTube's powerful search function to deliver the best results.</p>
                     <p className="description">View details about video view counts, like and dislike counts, amounts of comments, video lengths, channel subscriber counts, and more.</p>
 
-                    <p className="ready-to-try">Ready to try it?</p>
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text">Ready to try it?</p>
+                    <a className="button" href="/invite">Invite Eutenly</a>
 
                 </div>
 
@@ -159,8 +159,8 @@ export default () => (
                     <p className="description">Search Twitter to view tweets, profiles, and hashtags. Eutenly gets data straight from Twitter to display up-to-date information right in Discord.</p>
                     <p className="description">Explore Twitter however you'd like. Maybe start with a search, view a tweet, check the replies, go to someone's profile, etc.</p>
 
-                    <p className="ready-to-try">Ready to try it?</p>
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text">Ready to try it?</p>
+                    <a className="button" href="/invite">Invite Eutenly</a>
 
                 </div>
 
@@ -182,8 +182,8 @@ export default () => (
                     <p className="description">The Reddit Command lets you search for subreddits, posts, and users right from Discord. Eutenly embeds search results in a neat list for you to explore.</p>
                     <p className="description">Browse content the way you want. The Reddit command is continuous in a way that you can view a post's replies, visit someone's profile, view their posts, etc.</p>
 
-                    <p className="ready-to-try">Ready to try it?</p>
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text">Ready to try it?</p>
+                    <a className="button" href="/invite">Invite Eutenly</a>
 
                 </div>
 
@@ -205,8 +205,8 @@ export default () => (
                     <p className="description">Save search results and links with Eutenly so you can easily access them later. Enter links yourself or save search results directly from other commands.</p>
                     <p className="description">Saved links can be accessed whenever you'd like, and are displayed in neatly organized pages for easy viewing.</p>
 
-                    <p className="ready-to-try highlight">Ready to try it?</p>
-                    <a className="invite-button highlight" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text highlight">Ready to try it?</p>
+                    <a className="button highlight" href="/invite">Invite Eutenly</a>
 
                 </div>
 
@@ -233,8 +233,8 @@ export default () => (
                         <li>Domain Name Search</li>
                     </ul>
 
-                    <p className="ready-to-try">Ready to try it?</p>
-                    <a className="invite-button" href="/invite">Invite Eutenly</a>
+                    <p className="ready-text">Ready to try it?</p>
+                    <a className="button" href="/invite">Invite Eutenly</a>
 
                 </div>
             </div>
---
+++ pages/voter-perks.jsx
@@ -0,0 +1,219 @@
+import Head from "../components/Head";
+import NavBar from "../components/NavBar";
+import "./global.scss";
+
+export default () => (
+    <div id="index">
+
+        <Head
+            title="Voter Perks - Eutenly"
+            description="Vote for Eutenly on Top.GG and get some sweet perks"
+        />
+        <NavBar />
+
+        <div className="intro">
+
+            <div className="background-effect" />
+
+            <div className="about">
+                <div className="about-content">
+
+                    <h1 className="title">Voter Perks</h1>
+                    <p className="info">Vote for Eutenly on Top.GG</p>
+
+                    <div className="divider" />
+
+                    <p className="description">Voting for Eutenly on Top.GG will give you some perks to expand how you can use the bot.</p>
+                    <p className="description">Note that votes expire after 12 hours. To keep your perks, you just need to vote again.</p>
+
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+            </div>
+
+            <div className="intro-features-wrapper">
+                <div className="intro-features">
+
+                    <p className="title">Enhance Your Experience</p>
+
+                    <div className="intro-feature">
+
+                        <div className="name">
+                            <div className="divider" />
+                            <img className="icon" src="/assets/google.svg" />
+                            <p className="text">Google Knowledge Panels</p>
+                            <div className="divider" />
+                        </div>
+
+                        <p className="learn-more" onClick={() => window.scrollTo({
+                            top: document.querySelectorAll(".features .feature")[0].getBoundingClientRect().top + window.pageYOffset - 100,
+                            behavior: "smooth"
+                        })}>Learn More</p>
+
+                    </div>
+
+                    <div className="intro-feature">
+
+                        <div className="name">
+                            <div className="divider" />
+                            <img className="icon" src="/assets/twitter.svg" />
+                            <p className="text">Twitter Feeds &amp; Actions</p>
+                            <div className="divider" />
+                        </div>
+
+                        <p className="learn-more" onClick={() => window.scrollTo({
+                            top: document.querySelectorAll(".features .feature")[1].getBoundingClientRect().top + window.pageYOffset - 100,
+                            behavior: "smooth"
+                        })}>Learn More</p>
+
+                    </div>
+
+                    <div className="intro-feature">
+
+                        <div className="name">
+                            <div className="divider" />
+                            <img className="icon" src="/assets/reddit.svg" />
+                            <p className="text">Reddit Actions</p>
+                            <div className="divider" />
+                        </div>
+
+                        <p className="learn-more" onClick={() => window.scrollTo({
+                            top: document.querySelectorAll(".features .feature")[2].getBoundingClientRect().top + window.pageYOffset - 100,
+                            behavior: "smooth"
+                        })}>Learn More</p>
+
+                    </div>
+
+                    <p className="more-features" onClick={() => window.scrollTo({
+                        top: document.querySelector(".features").getBoundingClientRect().top + window.pageYOffset - 100,
+                        behavior: "smooth"
+                    })}>More Features</p>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div className="features">
+
+            <p className="title">Features</p>
+
+            <div className="feature">
+
+                <div className="content">
+
+                    <div className="name">
+                        <div className="divider left" />
+                        <img className="icon" src="/assets/google.svg" />
+                        <p className="text">Google Knowledge Panels</p>
+                        <div className="divider" />
+                    </div>
+
+                    <p className="description">Google Knowledge Panels allow you to view a lot of information in a single embed about key things like people, music, companies, and more.</p>
+                    <p className="description">Knowledge Panels include people's social profiles, companies' products, music producers' albums, and music's lyrics</p>
+
+                    <p className="ready-text">Ready to vote?</p>
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+
+                <img className="image" src="/assets/google-command.png" alt="An example of the Google command's result" />
+
+            </div>
+
+            <div className="feature right">
+
+                <div className="content">
+
+                    <div className="name">
+                        <div className="divider left" />
+                        <img className="icon" src="/assets/twitter.svg" />
+                        <p className="text">Twitter Feeds &amp; Actions</p>
+                        <div className="divider" />
+                    </div>
+
+                    <p className="description">View your personalized Twitter feed right in Discord. Tweets will be displayed similar to how you would see them on Twitter.</p>
+                    <p className="description">When viewing tweets, you can also like and retweet tweets. Eutenly's rich display of information provides an easy way to view content on Discord.</p>
+
+                    <p className="ready-text">Ready to vote?</p>
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+
+                <img className="image right" src="/assets/youtube-command.png" alt="An example of the YouTube command's result" />
+
+            </div>
+
+            <div className="feature">
+
+                <div className="content">
+
+                    <div className="name">
+                        <div className="divider left" />
+                        <img className="icon" src="/assets/reddit.svg" />
+                        <p className="text">Reddit Actions</p>
+                        <div className="divider" />
+                    </div>
+
+                    <p className="description">While browsing Reddit, enjoy the added benefit of being able to upvote and downvote posts right from Discord. Voting is as simple as reacting to a message.</p>
+                    <p className="description">Eutenly also supports saving posts and viewing your saved posts. While viewing a post, add a reaction to save it to your Reddit account.</p>
+
+                    <p className="ready-text">Ready to vote?</p>
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+
+                <img className="image" src="/assets/google-command.png" alt="An example of the Google command's result" />
+
+            </div>
+
+            <div className="feature right">
+
+                <div className="content">
+
+                    <div className="name">
+                        <div className="divider left" />
+                        <img className="icon" src="/assets/link.svg" />
+                        <p className="text">More Saved Links</p>
+                        <div className="divider" />
+                    </div>
+
+                    <p className="description">Saved Links allow you to bookmark search results and links so you can quickly access them in the future. As a voter, your limit for the amount of posts you can save doubles from 15 to 30.</p>
+                    <p className="description">You can view your saved links anytime in organized pages for easy viewing.</p>
+
+                    <p className="ready-text">Ready to vote?</p>
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+
+                <img className="image right" src="/assets/youtube-command.png" alt="An example of the YouTube command's result" />
+
+            </div>
+
+            <div className="feature">
+
+                <div className="content">
+
+                    <div className="name">
+                        <div className="divider left" />
+                        <img className="icon" src="/assets/spotify.svg" />
+                        <p className="text">Modify Spotify Playlists</p>
+                        <div className="divider" />
+                    </div>
+
+                    <p className="description">Found a song that you like? Have Eutenly add it to a playlist with a simple command to easily be able to listen to it again.</p>
+                    <p className="description">You'll also be able to view your playlists, quickly get details about songs and artists, and even remove songs.</p>
+
+                    <p className="ready-text">Ready to vote?</p>
+                    <a className="button" href="/vote" target="_blank">Vote</a>
+
+                </div>
+
+                <img className="image" src="/assets/twitter-command.png" alt="An example of the Twitter command's result" />
+
+            </div>
+
+        </div>
+
+    </div>
+);
