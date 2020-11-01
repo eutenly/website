646195ea9b5f5a2a4fa3b69bc04a8262b786f7ae
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-01 08:46:27 -0800

    Fixed bug with `/connections` page not redirecting to discord login

--- src/api/getConnections.js
+++ src/api/getConnections.js
@@ -4,5 +4,7 @@ import endpoint from "./endpoint";
 export default async function getConnections() {
 
     // Fetch
-    return await axios.get(endpoint("me"));
+    return await axios.get(endpoint("me", {
+        validateStatus: status => status < 500
+    }));
 }
