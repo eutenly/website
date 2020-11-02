b7a95066360c5fa757f8b4d28446c51226eb3b61
Maia <zgovorovski@gmail.com>
2020-11-02 19:13:58 +0000

    Hotfix for weird Gatsby hydration issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default class Footer extends React.Component {
 
             <Link className="home-button" to="/"><img src="/assets/text-logo.svg" alt="Eutenly logo" /></Link>
 
-            <p className="copyright">Copyright &copy; {new Date().getFullYear()} <a href="https://apixel.me">APixel Visuals</a> and <a href="https://maia.codes">Maia</a></p>
+            <p className="copyright">Copyright &copy; {new Date().getFullYear()} <a id="apixel" href="https://apixel.me">APixel Visuals</a> and <a id="maiacodes" href="https://maia.codes">Maia</a></p>
 
             <p className="links"><Link to="/terms-of-service">Terms of Service</Link> &bull; <Link to="/privacy-policy">Privacy Policy</Link></p>
             <p className="affiliation">Eutenly is not affiliated with Discord or any 3rd party service we integrate with.</p>
