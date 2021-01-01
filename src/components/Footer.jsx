b4868ccee4db0d00645efdeaf77935a7bd51b3ca
Maia <zgovorovski@gmail.com>
2021-01-01 14:43:38 +0000

    Footer pre-generates Year using gatsby

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "gatsby";
+import {graphql, Link, StaticQuery} from "gatsby";
 import "../styles/footer.scss";
 
-export default class Footer extends React.Component {
+class Footer extends React.Component {
 
     render = () => (
         <div id="footer">
 
             <Link className="home-button" to="/"><img src="/assets/text-logo.svg" alt="Eutenly logo" /></Link>
 
-            <p className="copyright">Copyright &copy; 2020 <a id="apixel" href="https://apixel.me">APixel Visuals</a> and <a id="maiacodes" href="https://maia.codes">Maia</a></p>
+            <p className="copyright">Copyright &copy; {this.props.year} <a id="apixel" href="https://apixel.me">APixel Visuals</a> and <a id="maiacodes" href="https://maia.codes">Maia</a></p>
 
             <p className="links"><Link to="/terms-of-service">Terms of Service</Link> &bull; <Link to="/privacy-policy">Privacy Policy</Link></p>
             <p className="affiliation">Eutenly is not affiliated with Discord or any 3rd party service we integrate with.</p>
@@ -17,3 +17,13 @@ export default class Footer extends React.Component {
         </div>
     );
 };
+
+export default () => (
+    <StaticQuery query={graphql`
+        {
+          site {
+            buildTime(formatString: "YYYY")
+          }
+        }
+    `} render={(data) => ( <Footer year={data.site.buildTime} /> )}/>
+)
